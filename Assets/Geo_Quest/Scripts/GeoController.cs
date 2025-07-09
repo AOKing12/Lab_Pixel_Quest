d1ef0483a6e7267bef859ae96f80540537785521
AOKing12 <aarono59@nycstudents.net>
2025-07-09 11:28:40 -0400

    Made 2 strings

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -1,15 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.Collections.LowLevel.Unsafe;
-using UnityEditor;
+
 using UnityEngine;
 
 public class NewScript : MonoBehaviour
 {
+    string variableone = ("Hello ");
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(“Hello World”);
+        string variabletwo = ("World");
+        Debug.Log(variableone +variabletwo);
     }
 
     // Update is called once per frame
