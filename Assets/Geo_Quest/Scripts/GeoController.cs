05c2a868f015618fbb4ba4a559b3f5626bcd7967
AOKing12 <aarono59@nycstudents.net>
2025-07-09 10:38:37 -0400

    First code

---
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEditor;
+using UnityEngine;
+
+public class NewScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(“Hello World”);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
