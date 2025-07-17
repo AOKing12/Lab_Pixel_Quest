04621f3f8240337c191126729197a29823f5133a
AOKing12 <aarono59@nycstudents.net>
2025-07-17 12:08:28 -0400

    7/17
    Added code for player with a custom sprite. Can move and the camera moves along with it.

---
+++ Assets/Pixel_Quest/Scripts/PlayerMove.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMov : MonoBehaviour
+{
+
+    private Rigidbody2D _rigidbody2D;
+    private SpriteRenderer _spriteRenderer;
+    public int speed = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _spriteRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMovement = Input.GetAxis("Horizontal");
+
+        _rigidbody2D.velocity = new Vector2(xMovement * speed, _rigidbody2D.velocity.y);
+
+        if (xMovement > 0) { _spriteRenderer.flipX = true; }
+        else if (xMovement < 0) { _spriteRenderer.flipX = false; }
+    }
+
+}
